6b485562eb9b13bb845770f0b4c9eff0e426ebfb
devinderchawla1981 <128513047+devinderchawla1981@users.noreply.github.com>
2023-03-21 17:30:57 +0000

    Update first_streamlist_app.py
    Added items and emojis

--- first_streamlist_app.py
+++ first_streamlist_app.py
@@ -2,7 +2,9 @@ import streamlit
 
 streamlit.title("My parents new dinner")
 
-streamlit.header('Breakfast Menu')
-streamlit.text('Omega 3 & Blueberry Oatmeal')
-streamlit.text('Kale, Spinach & Rocket Smoothie')
-streamlit.text('Hard-Boiled Free-Range Egg')
+streamlit.header('Breakfast Favourites')
+streamlit.text('🥣Omega 3 & Blueberry Oatmeal')
+streamlit.text('🥗Kale, Spinach & Rocket Smoothie')
+streamlit.text('🐔Hard-Boiled Free-Range Egg')
+streamlist.text('🥑🍞 Avocado Toast')
+
