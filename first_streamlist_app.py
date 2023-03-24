e579cc06885bd847ce75f1a89716bbd33f68995e
devinderchawla1981 <128513047+devinderchawla1981@users.noreply.github.com>
2023-03-24 17:45:16 +0000

    Update first_streamlist_app.py

--- first_streamlist_app.py
+++ first_streamlist_app.py
@@ -44,7 +44,7 @@ streamlit.dataframe(fruityvice_normalized)
 my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
 my_cur = my_cnx.cursor()
 my_cur.execute("select * from  PC_RIVERY_DB.PUBLIC.fruit_load_list")
-my_data_row = my_cur.fetchone()
+my_data_row = my_cur.fetchall()
 streamlit.text("Hello from Snowflake:")
 streamlit.dataframe(my_data_row)
 
