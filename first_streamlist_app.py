2dc9cfd0fdf39488cac5f8fea9024e77d74c1761
devinderchawla1981 <128513047+devinderchawla1981@users.noreply.github.com>
2023-03-25 06:55:49 +0000

    Update first_streamlist_app.py

--- first_streamlist_app.py
+++ first_streamlist_app.py
@@ -60,7 +60,7 @@ def get_fruit_list() :
 streamlit.text("Hello from Snowflake:")
 if streamlit.button('Get Fruit Load List'):
   my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
-  my_data_rows=get_fruit_list 
+  my_data_rows=get_fruit_list() 
   streamlit.dataframe(my_data_rows)
 
 def insert_new_rows(new_fruit):
