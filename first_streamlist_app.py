471161dc727ceace188666743756ce17f045b160
devinderchawla1981 <128513047+devinderchawla1981@users.noreply.github.com>
2023-03-25 06:39:39 +0000

    Update first_streamlist_app.py

--- first_streamlist_app.py
+++ first_streamlist_app.py
@@ -50,16 +50,19 @@ try:
 except URLError as e:
   streamlist.error()
 
+  
+def get_fruit_list(my_cnx):
+  my_cur = my_cnx.cursor()
+  my_cur.execute("select * from  PC_RIVERY_DB.PUBLIC.fruit_load_list")
+  my_data_rows = my_cur.fetchall()
+  return my_data_rows
 
-streamlit.stop()
-my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
-my_cur = my_cnx.cursor()
-my_cur.execute("select * from  PC_RIVERY_DB.PUBLIC.fruit_load_list")
-my_data_rows = my_cur.fetchall()
 streamlit.text("Hello from Snowflake:")
+my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
+my_data_rows=get_fruit_list(my_cnx)
 streamlit.dataframe(my_data_rows)
 
-
+streamlit.stop()
 my_cur.execute("insert into PC_RIVERY_DB.PUBLIC.FRUIT_LOAD_LIST values ('from streamlit')")
 
 
