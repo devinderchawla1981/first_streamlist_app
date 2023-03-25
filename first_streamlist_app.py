5a8c5573fd8b6250fe7b5a89270f15ca665894f3
devinderchawla1981 <128513047+devinderchawla1981@users.noreply.github.com>
2023-03-25 06:17:10 +0000

    Update first_streamlist_app.py

--- first_streamlist_app.py
+++ first_streamlist_app.py
@@ -4,7 +4,8 @@ import streamlit
 
 import pandas
 
-import urllib.error import URLError
+import urllib.error 
+import URLError
 
 
 
@@ -25,9 +26,7 @@ my_fruit_list = my_fruit_list.set_index('Fruit')
 # Let's put a pick list here so they can pick the fruit they want to include 
 fruits_selected=streamlit.multiselect("Pick some fruits:", list(my_fruit_list.index),['Avocado','Strawberries'])
 fruits_to_show = my_fruit_list.loc[fruits_selected]
-
 # Display the table on the page.
-
 streamlit.dataframe(fruits_to_show)
 
 fruit_choice = streamlit.text_input('What fruit would you like information about?','Kiwi')
